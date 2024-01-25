817e42ab72c063d2105a23a51a7003e1859ceb50
yakupeyisan <47880402+yakupeyisan@users.noreply.github.com>
2024-01-25 22:33:37 +0300

    First Initialize

--- src/Projects/Uroflow/Uroflow.WebAPI/Program.cs
+++ src/Projects/Uroflow/Uroflow.WebAPI/Program.cs
@@ -4,13 +4,6 @@ using Uroflow.Persistance;
 using Core.Extensions.SystemExtensions;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
